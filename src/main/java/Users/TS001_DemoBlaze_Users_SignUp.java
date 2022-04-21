16841d828bb9e27d8aadae4eafe0e8909a4adfb7
LeopMora <104149120+LeopMora@users.noreply.github.com>
2022-04-21 12:30:08 -0500

    Update TS001_DemoBlaze_Users_SignUp.java
    Add message

--- src/main/java/Users/TS001_DemoBlaze_Users_SignUp.java
+++ src/main/java/Users/TS001_DemoBlaze_Users_SignUp.java
@@ -4,6 +4,6 @@ import base.TS000_DemoBlaze_Base;
 
 public class TS001_DemoBlaze_Users_SignUp extends TS000_DemoBlaze_Base{
 	public TS001_DemoBlaze_Users_SignUp() {
-		
+		System.out.println("Hola");
 	}
 }
